e84c9b69eded4e50d6babd4bfd577a6d3bbf0409
KarimmYasser <Karimmyasserr@gmail.com>
2024-04-21 12:02:03 +0200

    Karim's work

---
+++ HexConverter.v
@@ -0,0 +1,25 @@
+module HexConverter (in, converted);
+	input [3:0] in;
+	output [6:0] converted;
+    always
+        begin
+	       case(in)
+            4'h0: converted[6:0] = 7'b0111111;
+            4'h1: converted[6:0] = 7'b0000110;
+            4'h2: converted[6:0] = 7'b1011011;
+            4'h3: converted[6:0] = 7'b1001111;
+            4'h4: converted[6:0] = 7'b1100110;
+            4'h5: converted[6:0] = 7'b1101101;
+            4'h6: converted[6:0] = 7'b1111101;
+            4'h7: converted[6:0] = 7'b0000111;
+            4'h8: converted[6:0] = 7'b1111111;
+            4'h9: converted[6:0] = 7'b1101111;
+            4'hA: converted[6:0] = 7'b1110111;
+            4'hB: converted[6:0] = 7'b1111100;
+            4'hC: converted[6:0] = 7'b1011000;
+            4'hD: converted[6:0] = 7'b1011110;
+            4'hE: converted[6:0] = 7'b1111001;
+            default : converted[6:0] = 7'b1110001;
+           endcase
+        end
+endmodule
---
+++ mixColumns.v
@@ -0,0 +1,36 @@
+module mixColumns(state_in,state_out);
+
+input [0:127] state_in;
+output[0:127] state_out;
+
+function [7:0] mul02;
+	input [7:0] x;
+	begin 
+			if(x[7] == 1) mul02 = ((x << 1) ^ 8'h1b);
+			else mul02 = x << 1; 
+	end 	
+endfunction
+
+
+function [7:0] mul03;
+	input [7:0] x;
+	begin 
+			
+			mul03 = mul02(x) ^ x;
+	end 
+endfunction
+
+
+genvar i;
+
+for(i=0;i< 4;i=i+1) begin
+
+assign state_out[i*32+:8]= mul02(state_in[i*32+:8]) ^ mul03(state_in[(i*32 + 8)+:8]) ^ state_in[(i*32 + 16)+:8] ^ state_in[(i*32 + 24)+:8];
+assign state_out[(i*32 + 8)+:8]= state_in[i*32+:8] ^ mul02(state_in[(i*32 + 8)+:8]) ^ mul03(state_in[(i*32 + 16)+:8]) ^ state_in[(i*32 + 24)+:8];
+assign state_out[(i*32 + 16)+:8]= state_in[i*32+:8] ^ state_in[(i*32 + 8)+:8] ^ mul02(state_in[(i*32 + 16)+:8]) ^ mul03(state_in[(i*32 + 24)+:8]);
+assign state_out[(i*32 + 24)+:8]= mul03(state_in[i*32+:8]) ^ state_in[(i*32 + 8)+:8] ^ state_in[(i*32 + 16)+:8] ^ mul02(state_in[(i*32 + 24)+:8]);
+
+end
+
+
+endmodule
