d808fcc8a291e5735e16269b3b3e6acbdc7a4e21
youssef wafa <youssef.mwafa@gmail.com>
2024-04-23 23:12:42 +0200

    KeyExpansion but dont merge yet

--- KeyExpansion.v
+++ KeyExpansion.v
@@ -1,49 +1,40 @@
-module KeyExpansion(input [0:127] key , output [0:1408] word);
+module KeyExpansion(input [127 : 0] key , output [1408 : 0] word);
 
-function [0:31] rcon;
-input integer x;
-begin
- case(x)
-    2: rcon=32'h01000000;
-    3: rcon=32'h02000000;
-    4: rcon=32'h04000000;
-    5: rcon=32'h08000000;
-    6: rcon=32'h10000000;
-    7: rcon=32'h20000000;
-    8: rcon=32'h40000000;
-    9: rcon=32'h80000000;
-    10: rcon=32'h1b000000;
-    11: rcon=32'h36000000;
-    default: rcon=32'h00000000;
-  endcase
-  end
-endfunction
+ 
+  assign word[127 : 0] = key[127 : 0];
 
-function [0:31] g;
-input [0:31] x;
-input integer rconi;
-begin
-    wire y;
-    x = {x[8:31],x[0:7]};
-    //SubBytes(x,y);
-    //g = y xor rcon
-    input rconx = rcon(rconi);
-    g = y ^ rconx;
-//input rcon = [ 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36];
-end
-endfunction
-assign word[0 : 127] = key[0 : 127];
-
-genvar i;
-generate
-for(i = 2 ; i <= 11 ; i = i + 1) begin
-
-    wire G = g(word[ (i-1)*128 + 96 : (i-1)*32 + 127]);
-   assign word[ i*128       : i*32 + 31 ] = word[ (i-1)*128       : (i-1)*32 + 31 ] ^ G ;
-   assign word[ i*128 + 32  : i*32 + 63 ] = word[ (i-1)*128 + 32  : (i-1)*32 + 63 ] ^ word[ i*128       : i*32 + 31 ];
-   assign word[ i*128 + 64  : i*32 + 95 ] = word[ (i-1)*128 + 64  : (i-1)*32 + 95 ] ^ word[ i*128 + 32  : i*32 + 63 ];
-   assign word[ i*128 + 96  : i*32 + 127] = word[ (i-1)*128 + 96  : (i-1)*32 + 127] ^ word[ i*128 + 64  : i*32 + 95 ];
-end
-
-endgenerate
+  genvar i;
+  generate
+    for (i = 1; i <= 10; i = i + 1) begin
+      wire [31:0] G;
+      g g1 (word[(i-1)*128 + 31 : (i-1)*128], i , G);
+      assign word[i*128 + 31  : i*128     ] = word[(i-1)*128 + 31  : (i-1)*128] ^ G;
+      assign word[i*128 + 63  : i*128 + 32] = word[(i-1)*128 + 63  : (i-1)*128 + 32] ^ word[i*128 + 31  : i*128     ];
+      assign word[i*128 + 95  : i*128 + 64] = word[(i-1)*128 + 95  : (i-1)*128 + 64] ^ word[i*128 + 63  : i*128 + 32];
+      assign word[i*128 + 127 : i*128 + 96] = word[(i-1)*128 + 127 : (i-1)*128 + 96] ^ word[i*128 + 95  : i*128 + 64];
+    end
+  endgenerate
 endmodule
+
+module getrcon(input integer x, output [31:0] rcon);
+    assign rcon = (x == 1)  ? 32'h01000000 :
+                  (x == 2)  ? 32'h02000000 :
+                  (x == 3)  ? 32'h04000000 :
+                  (x == 4)  ? 32'h08000000 :
+                  (x == 5)  ? 32'h10000000 :
+                  (x == 6)  ? 32'h20000000 :
+                  (x == 7)  ? 32'h40000000 :
+                  (x == 8)  ? 32'h80000000 :
+                  (x == 9)  ? 32'h1b000000 :
+                  (x == 10) ? 32'h36000000 :
+                              32'h00000000;
+endmodule
+
+
+module g (input [31:0] x, input integer rconi, output [31:0] out);
+      wire [31:0] shiftedx = {x[23:0], x[31:24]};
+      wire [31:0] rconx;
+      getrcon r1 (rconi , rconx);
+      assign out = shiftedx ^ rconx;
+endmodule
+
---
+++ SubBytes.v
@@ -0,0 +1,5 @@
+module SubBytes(input [0:31] in , output [0:31] out);
+
+
+
+endmodule
