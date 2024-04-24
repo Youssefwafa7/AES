61d25fae5e9fddaccb6a46fb8f2674ca33328c4e
youssef wafa <youssef.mwafa@gmail.com>
2024-04-24 21:29:13 +0200

    index fixed in for loop

--- KeyExpansion.v
+++ KeyExpansion.v
@@ -1,21 +1,21 @@
 module KeyExpansion(input [127 : 0] key , output [1407 : 0] word);
  
-  assign word[127 : 0] = key[127 : 0];
+  assign word[1407 : 1280] = key[127 : 0];
 
   genvar i;
   generate
     for (i = 1; i <= 10; i = i + 1) begin
       wire [31:0] G;
-      g g1 (word[(i-1)*128 + 31 : (i-1)*128], i , G);
-
-      assign word[i*128 + 31  : i*128     ] = word[(i-1)*128 + 127 : (i-1)*128 + 96] ^ G;
-      assign word[i*128 + 63  : i*128 + 32] = word[(i-1)*128 + 95  : (i-1)*128 + 64] ^ word[i*128 + 31  : i*128     ];
-      assign word[i*128 + 95  : i*128 + 64] = word[(i-1)*128 + 63  : (i-1)*128 + 32] ^ word[i*128 + 63  : i*128 + 32];
-      assign word[i*128 + 127 : i*128 + 96] = word[(i-1)*128 + 31  : (i-1)*128]^ word[i*128 + 95  : i*128 + 64];
+      g g1 (word[1407 - (i-1)*128 - 96 : 1407 - (i-1)*128 - 127], i , G);
+//1407-127
+      assign word[1407 - i*128	    : 1407 - i*128 - 31 ] = word[1407 - (i-1)*128	   : 1407 - (i-1)*128 - 31 ] ^ G;
+      assign word[1407 - i*128 - 32 : 1407 - i*128 - 63 ] = word[1407 - (i-1)*128 - 32 : 1407 - (i-1)*128 - 63 ] ^ word[1407 - i*128	  : 1407 - i*128 - 31 ];
+      assign word[1407 - i*128 - 64 : 1407 - i*128 - 95 ] = word[1407 - (i-1)*128 - 64 : 1407 - (i-1)*128 - 95 ] ^ word[1407 - i*128 - 32 : 1407 - i*128 - 63 ];
+      assign word[1407 - i*128 - 96 : 1407 - i*128 - 127] = word[1407 - (i-1)*128 - 96 : 1407 - (i-1)*128 - 127] ^ word[1407 - i*128 - 64 : 1407 - i*128 - 95 ];
     end
   endgenerate
 endmodule
-
+//2b7e1516 28aed2a6 abf71588 09cf4f3c a0fafe17 88542cb1 23a33939 2a6c7605 f2c295f2 7a96b943 5935807a 7359f67f 3d80477d 4716fe3e 1e237e44 6d7a883b ef44a541 a8525b7f b671253b db0bad00 d4d1c6f8 7c839d87 caf2b8bc 11f915bc 6d88a37a 110b3efd dbf98641 ca0093fd 4e54f70e 5f5fc9f3 84a64fb2 4ea6dc4f ead27321 b58dbad2 312bf560 7f8d292f ac7766f3 19fadc21 28d12941 575c006e d014f9a8 c9ee2589 e13f0cc8 b6630ca6
 module getrcon(input integer x, output [31:0] rcon);
     assign rcon = (x == 1)  ? 32'h01000000 :
                   (x == 2)  ? 32'h02000000 :
@@ -35,13 +35,13 @@ module g (input [31:0] x, input integer rconi, output [31:0] out);
       function [31:0] subwordx;
           input [31:0] a;
           begin
-          subwordx[31:24]=  c(a[31:24]);
+          subwordx[31:24]= c(a[31:24]);
           subwordx[23:16]= c(a[23:16]);
-          subwordx[15:8]=c(a[15:8]);
-          subwordx[7:0]=c(a[7:0]);
+          subwordx[15:8] = c(a[15:8]);
+          subwordx[7:0]  = c(a[7:0]);
           end
       endfunction
-      function [0:7] c(input [0:7] a);  
+      function [7:0] c(input [7:0] a);  
         begin
           case (a)
             8'h00: c=8'h63;
@@ -308,7 +308,6 @@ module g (input [31:0] x, input integer rconi, output [31:0] out);
       wire [31:0] rconx;
       wire [31:0] subx;
       assign subx = subwordx(shiftedx);
-      //sbox instead of subbytes
       getrcon r1 (rconi , rconx);
       assign out = subx ^ rconx;
 endmodule
