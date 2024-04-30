3e4ff95fa374e17fc1a9682a968da8353717c1aa
abdallah farag <abdallah_altalbawy@yahoo.com>
2024-04-30 19:48:25 +0300

    finally cipher is done

--- Main.v
+++ Main.v
@@ -1,9 +1,10 @@
-module main (input [127 : 0] in, input  [127 : 0] key , output [127 : 0] out);
-    assign in = 00112233445566778899aabbccddeeff;
-    assign key = 000102030405060708090a0b0c0d0e0f;
+
+module main (input clk,output [127 : 0] out);
+    wire [127:0] in = 128'h00112233445566778899aabbccddeeff;
+    wire [127:0] key = 128'h000102030405060708090a0b0c0d0e0f;
     wire [1407:0] words;
-    wire [127:0] out1;
+    wire [127:0] out188;
     KeyExpansion k1 (key,words);
-    Cipher c1 (in,words,out1);
-    assign out=out1;
+    Cipher c1 (in,words,clk,out188);
+    assign out=out188;
 endmodule
--- combined.v
+++ combined.v
@@ -13,30 +13,37 @@ module Cipher(input [127 : 0] in, input [1407 : 0] w , input clk ,output reg [12
    	wire [127 : 0] out1;
     wire [127 : 0] sub;
     wire [127 : 0] shift;
-    wire [127 : 0] mix;
     //wire [127 : 0] out2;
    // wire [127 : 0] out9;
-    integer i=1;
+    integer i=-1;
 	reg [1407:0] states;
-    wire [127 : 0] plzdontfuckme;
-
-    AddRoundKey k2 (in, w[1407 : 1280], states[1407-:128]);
-	encryptRound e9 (states [1407-(i-1)*128-:128] ,w[1407-i*128-:128],plzdontfuckme);
-	SubBytes sb(states[1407-i*128-:128],sub);
+    wire [127 : 0] out44;
+	wire [127:0] k222;
+    AddRoundKey k2 (in, w[1407 : 1280], k222);
+	encryptRound e9 (states [1407-((i)*128)-:128] ,w[1407-((i+1)*128)-:128],out44);
+	SubBytes sb(states[255:128],sub);
 	ShiftRows sr(sub,shift);
 	AddRoundKey addrk2(shift,w[127:0],out1);
 
+
 	always @ (posedge clk) begin 
-	states[1407-(i+1)*128-:128]<=plzdontfuckme;
-		end
-	always @ (posedge clk) begin 
-		if(i<=9)begin
-			out7 <= states[1407-i*128-:128];
-			i<=i+1;
-		end
-		else if (i>9)begin
-			out7 <= out1;
-		end
+		if(i<10)begin 
+				if(i==-1&& k222 !== 'bx)begin
+					states[1407-:128]<=k222;
+					out7 <= k222;
+					i=i+1;
+				end
+				else if(i<=8&& out44 !== 'bx)begin
+						states[1407-((i+1)*128)-:128]<=out44;
+						out7 <= out44;
+						i=i+1;
+					end 
+					else if(i==9&& out44 !== 'bx)begin
+						states[127:0]<=out44;
+						out7<=out1;
+					end
+
+		end	
 	end
 endmodule
 
@@ -48,12 +55,11 @@ wire [127:0] afterSubBytes;
 wire [127:0] keyout;
 wire [127:0] afterShiftRows;
 wire [127:0] afterMixColumns;
-wire [127:0] afterAddroundKey;
 
-SubBytes s(in,afterSubBytes);
-ShiftRows r(afterSubBytes,afterShiftRows);
-mixColumns m(afterShiftRows,afterMixColumns);
-AddRoundKey b(afterMixColumns,key,keyout);
+SubBytes s1(in,afterSubBytes);
+ShiftRows r1(afterSubBytes,afterShiftRows);
+mixColumns m32323(afterShiftRows,afterMixColumns);
+AddRoundKey k77(afterMixColumns,key,keyout);
 assign out = keyout;
 		
 endmodule
