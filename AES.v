f3a676b27d8f67594de147e2b4aafa2e82fad5ff
KarimmYasser <Karimmyasserr@gmail.com>
2024-05-09 14:10:26 +0300

    final product i guess

--- AES.v
+++ AES.v
@@ -16,26 +16,30 @@ module aes(input clk,input [1:0]SW,input reset,output[6:0]HEX0,output[6:0]HEX1,o
     wire [127:0] encrypted128;
     wire [127:0] encrypted192;
     wire [127:0] encrypted256;
-	reg enable = 0;
+	reg enable128 = 0;
+	reg enable192 = 0;
+	reg enable256 = 0;
     integer i = -1;
     integer Nr=10;
      always @(*) begin
-       	if (SW[0]) begin
+       	if (SW==1) begin
             Nr=12;
          end
-        else if (SW[1]) begin
+        else if (SW==2) begin
             Nr=14;
-        end
+		end else begin 
+			Nr=10;
     end
+	end
     KeyExpansion #(4,10) k10985 (key128,words128);
     KeyExpansion #(6,12) k223412we (key192,words192);
     KeyExpansion #(8,14) k3234sa (key256,words256);
     Cipher #(4,10) c1234sad (in,words128,clk,reset,encrypted128);
     Cipher #(6,12) c2234ad (in,words192,clk,reset,encrypted192);
     Cipher #(8,14) c32134sdf (in,words256,clk,reset,encrypted256);
-    DeCipher #(4,10) dc1234asf (encrypted128,words128,clk,reset,enable,decrypted128);
-    DeCipher #(6,12) dc2234sdf (encrypted192,words192,clk,reset,enable,decrypted192);
-  	DeCipher #(8,14) dc324sdf (encrypted256,words256,clk,reset,enable,decrypted256);
+    DeCipher #(4,10) dc1234asf (encrypted128,words128,clk,reset,enable128,decrypted128);
+    DeCipher #(6,12) dc2234sdf (encrypted192,words192,clk,reset,enable192,decrypted192);
+  	DeCipher #(8,14) dc324sdf (encrypted256,words256,clk,reset,enable256,decrypted256);
     assign out128=(i<Nr+1)?encrypted128:decrypted128;
     assign out192=(i<Nr+1)?encrypted192:decrypted192;
    	assign out256=(i<Nr+1)?encrypted256:decrypted256;
@@ -43,10 +47,19 @@ module aes(input clk,input [1:0]SW,input reset,output[6:0]HEX0,output[6:0]HEX1,o
     always@(negedge clk, posedge reset) begin
 		if(reset) begin 
 			i = -1;
+			enable128=0;
+			enable192=0;
+			enable256=0;
 		end
-        else if(i<2*(Nr+1)) begin
-                 if(i==Nr-1)begin
-					enable = 1;
+        else if(i<30)) begin
+                 if(i==9)begin
+					enable128 = 1;
+				 end
+				 else if(i==11)begin
+					enable192 = 1;
+				 end
+				 else if(i==13)begin
+					enable256 = 1;
 				 end
             i = i + 1;
             end
